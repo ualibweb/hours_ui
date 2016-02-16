a5e93e029d1e982e711094eb77b6a3206d99e821
bvkrylov <bvkrylov@ua.edu>
2016-02-16 12:06:38 -0600

    Changed today hours according to API changes, noe more place

--- src/app/list/list.js
+++ src/app/list/list.js
@@ -6,7 +6,7 @@ angular.module('hours.list', [])
 
         hoursFactory.get({view: 'today'},
             function(data){
-                var list = setStatus(data.libraries);
+                var list = setStatus(data.today.libraries);
                 $scope.hoursList = list;
             },
             function(msg){
