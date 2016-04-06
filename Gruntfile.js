02abfa88ed54b4b04b14e8c90e3d5a3ef16741e0
Will Jones <the8bitsquid@gmail.com>
2016-04-06 14:41:14 -0500

    Changed LESS imports for style variables. Added watch and livereload grunt tasks. Removed bowerrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt){
     // Load all tasks
     require('load-grunt-tasks')(grunt);
+    var serveStatic = require('serve-static');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -19,13 +20,6 @@ module.exports = function(grunt){
             app: {
                 src: ['tmp/templates.js', 'src/app/**/*.js'],
                 dest: 'dist/hours.js'
-            },
-            index: {
-                src: 'src/index.html',
-                dest: 'dist/index.html',
-                options: {
-                    process: true
-                }
             }
         },
         less: {
@@ -63,6 +57,17 @@ module.exports = function(grunt){
             }
         },
         copy: {
+            demo: {
+                options: {
+                    processContent: function (content, srcpath) {
+                        return grunt.template.process(content);
+                    }
+                },
+                files: [{
+                    src: 'src/index.html',
+                    dest: 'dist/index.html'
+                }]
+            },
             vendor: {
                 files: [
                     {
@@ -82,6 +87,123 @@ module.exports = function(grunt){
         },
         clean: {
             app: ['tmp/']
+        },
+        watch: {
+            less: {
+                files: ['src/**/*.less'],
+                tasks: ['less:dev']
+            },
+            ng: {
+                files: ['src/**/*.js', 'src/**/*.tpl.html'],
+                tasks: ['html2js', 'concat', 'clean', 'dev_prod_switch:dev']
+            },
+            index: {
+                files: ['src/index.html'],
+                tasks: ['copy:index', 'dev_prod_switch:dev']
+            },
+            livereload: {
+                // Here we watch the files the sass task will compile to
+                // These files are sent to the live reload server after sass compiles to them
+                options: { livereload: true },
+                files: ['dist/**/*', 'docs/**/*']
+            }
+        },
+        connect: {
+            live: {
+                options: {
+                    open: true,
+                    keepalive: true,
+                    hostname: 'localhost',
+                    base: {
+                        path: 'dist',
+                        options: {
+                            index: 'index.html'
+                        }
+                    },
+                    middleware: function(connect) {
+                        return [
+                            serveStatic('.tmp'),
+                            connect().use('/bower_components', serveStatic('./bower_components')),
+                            serveStatic('./dist')
+                        ];
+                    }
+                }
+            },
+            dev: {
+                options: {
+                    livereload: true,
+                    open: true,
+                    hostname: 'localhost',
+                    base: {
+                        path: 'dist',
+                        options: {
+                            index: 'index.html'
+                        }
+                    },
+                    middleware: function(connect) {
+                        return [
+                            serveStatic('.tmp'),
+                            connect().use('/bower_components', serveStatic('./bower_components')),
+                            serveStatic('./dist')
+                        ];
+                    }
+                }
+            },
+            docs: {
+                options: {
+                    livereload: true,
+                    open: true,
+                    hostname: 'localhost',
+                    base: {
+                        path: 'docs',
+                        options: {
+                            index: 'index.html'
+                        }
+                    }
+                }
+            }
+        },
+        dev_prod_switch: {
+            dev: {
+                options: {
+                    environment: 'dev'
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html'
+                }
+            },
+            live: {
+                options: {
+                    environment: 'prod'
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html'
+                }
+            }
+        },
+        wiredep: {
+            demo: {
+                src: [
+                    'src/index.html'
+                ],
+                ignorePath: '../',
+                options: {
+                    devDependencies: true
+                },
+                fileTypes: {
+                    html: {
+                        block: /(([ \t]*)<!--\s*bower:*(\S*)\s*-->)(\n|\r|.)*?(<!--\s*endbower\s*-->)/gi,
+                        detect: {
+                            js: /<script.*src=['"]([^'"]+)/gi,
+                            css: /<link.*href=['"]([^'"]+)/gi
+                        },
+                        replace: {
+                            js: '<script src="/{{filePath}}"></script>',
+                            css: '<link rel="stylesheet" href="/{{filePath}}">'
+                        }
+                    }
+                }
+            }
         },
         bump: {
             options: {
@@ -103,6 +225,29 @@ module.exports = function(grunt){
         }
     });
 
-    grunt.registerTask('default', ['html2js', 'concat', 'less:dev', 'copy', 'clean']);
-    grunt.registerTask('live-build', ['default', 'less:build', 'ngAnnotate', 'uglify']);
+    grunt.registerTask('default', [
+        'dev-build', 'connect:dev', 'watch'
+    ]);
+    grunt.registerTask('dev-build', [
+        'html2js',
+        'concat',
+        'less:dev',
+        'copy',
+        'clean',
+        'dev_prod_switch:dev'
+    ]);
+    grunt.registerTask('live-build', [
+        'html2js',
+        'concat',
+        'less:dev',
+        'copy',
+        'clean',
+        'less:build',
+        'ngAnnotate',
+        'uglify',
+        'dev_prod_switch:live'
+    ]);
+    //TODO: add ng-docs and gh-pages tasks to this repo
+    //grunt.registerTask('docs', ['connect:docs', 'watch']);
+    grunt.registerTask('demo-live', ['live-build', 'connect:live']);
 };
--- README.md
+++ README.md
@@ -1,6 +1,52 @@
 hours_ui
 ========
 
-UI for the University Libraries hours web app
+> Directives and UI for the University Libraries hours web app
 
-Currently only the `hours-list` and `hours-calendar` directives are active.
+## Getting Started
+
+# Building the oneSearch UI
+
+## First Steps
+
+You need [Node.js](http://nodejs.org/) to use the tools that build this app. Download the proper Node.js package from their [download page](http://nodejs.org/download/).
+
+After Node.js is installed, then you need to install [Grunt.js](http://gruntjs.com/getting-started) and [Bower](http://bower.io/) via the Node Package Manager (npm).
+To do so, run the following commands:
+
+> If you already have the Grunt Command Line Interface and Bower installed globally, skip this step.
+
+```bash
+npm install -g grunt-cli
+npm install -g bower
+```
+
+## Install Grunt dependencies
+If you haven't used [grunt](http://gruntjs.com) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide.
+
+From the same directory as the repository's [Gruntfile.js](http://gruntjs.com/getting-started#the-gruntfile) and [package.json](http://gruntjs.com/getting-started#package.json), run the following command:
+
+```bash
+npm install
+```
+
+## Install Bower dependencies
+
+```bash
+bower install
+```
+
+## Edit and test locally before committing
+
+This repo's default Grunt task will `watch` the source files for changes and `livereload` your browser. 
+This will open a demo page of the app in your browser, re-build if any changes are made to the source, and `livereload` (refresh) your browser automatically.
+
+> The dev/live builds and the app's demo/test page will be built to the `/dist` folder.
+
+1. Run this command before making changes - the app's local demo page to open automatically in your browser.
+
+    ```bash
+    grunt
+    ```
+    
+2. Edit the source and either press `Ctrl` + `S` or wait a couple seconds; the changes will re-build and your browser refreshed automatically.
